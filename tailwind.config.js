e3b0fb25af6b9aa38816945ae0ab17d52fb76c69
akgunberk <akgunberk@yahoo.com>
2023-07-24 19:32:44 +0300

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      "dark-grey": "#696969",
+      "light-blue": "#adb9c3",
+      "dark-blue": "#3878a2",
+    },
+  },
+  plugins: [],
+};
